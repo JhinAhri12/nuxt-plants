a6e5da34ba7a94d073bcbbd31d001be6dc1255b2
JhinAhri12 <cyprien.rezki@hotmail.fr>
2022-09-26 12:56:43 +0200

    add plant directory and dynamic page

---
+++ pages/plant/_id.vue
@@ -0,0 +1,26 @@
+  <template>
+      <div>
+        <h1>Plant #{{ id }}</h1>
+      </div>
+    </template>
+    <script>
+    export default {
+      head() {
+        return {
+          title: 'Plant #' + this.id,
+          meta: [
+            {
+              hid: 'description',
+              name: 'description',
+              content: 'What you need to know about Plant #' + this.id
+            }
+          ]
+        }
+      },
+      computed: {
+        id() {
+          return this.$route.params.id
+        }
+      }
+    }
+    </script>
