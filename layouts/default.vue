2736976ce2eaed77147720264aea267d0c133c70
JhinAhri12 <cyprien.rezki@hotmail.fr>
2022-09-26 11:57:57 +0200

    add Navbar on Layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
+    <NavBar/>
     <nuxt />
   </div>
 </template>
-
+  <script>
+    import NavBar from "~/components/NavBar.vue";
+    export default {
+      components: {
+        NavBar
+      }
+    };
+    </script>
 <style>
 html {
   -webkit-text-size-adjust: 100%;
